9543dac60315dda2ab231398ac49f8ccf43a9051
Azizuddin Mohammed <azizuddin.rahil123@gmail.com>
2025-07-01 11:14:23 +0530

    working on nodes. nodes.legth is 163.

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const button = document.getElementById("check-button");
+
+button.addEventListener("click", valiadte);
+
+function valiadte() {
+  document.getElementById();
+}
+const elements = document.getElementsByClassName("container");
+console.log(elements);
+
+const printTheValues = document.getElementById("table").childNodes.length;
+console.log(printTheValues);
+
+//function valiadte() {
+//  document.getElementsByClassName("container").innerHTML = 0;
+//}
